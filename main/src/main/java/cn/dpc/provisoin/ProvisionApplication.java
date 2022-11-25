a945b8deb86c9daa3d7785a84427817491429e0a
peichao.dong <peichao.dong@thoughtworks.com>
2022-11-25 10:36:29 +0800

    init project and add domain

---
+++ main/src/main/java/cn/dpc/provisoin/ProvisionApplication.java
@@ -0,0 +1,12 @@
+package cn.dpc.provisoin;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvisionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProvisionApplication.class, args);
+    }
+
+}
