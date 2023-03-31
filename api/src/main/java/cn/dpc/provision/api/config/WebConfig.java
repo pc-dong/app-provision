09accca41b99a471d54ddc0a760c51b6647fc7c4
peichao.dong <peichao.dong@thoughtworks.com>
2023-03-31 17:32:10 +0800

    add featureFlag API

---
+++ api/src/main/java/cn/dpc/provision/api/config/WebConfig.java
@@ -0,0 +1,19 @@
+package cn.dpc.provision.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+@EnableWebFlux
+public class WebConfig implements WebFluxConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+    }
+}
+
